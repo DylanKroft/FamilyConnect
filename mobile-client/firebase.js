2fdb02f8c78209fc04039b4c991af5ffd0614f35
DylanKroft <dylankroft@gmail.com>
2022-11-03 20:54:09 +1100

    Update firebase.js

--- mobile-client/firebase.js
+++ mobile-client/firebase.js
@@ -12,13 +12,7 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDhKzDHg5o2_891FTSbGwr2RsCwM-cehGM",
-  authDomain: "familyconnect-a56a0.firebaseapp.com",
-  projectId: "familyconnect-a56a0",
-  storageBucket: "gs://familyconnect-a56a0.appspot.com",
-  messagingSenderId: "352525739703",
-  appId: "1:352525739703:web:50f3611584192ea68a0f32",
-  measurementId: "G-6Q1VR8KNBG"
+  //removed to allow this repo to be set to public
 };
 
 // Initialize Firebase
