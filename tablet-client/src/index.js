f0b277a36ee6758792bb5323ebf685a359434036
DylanKroft <dylankroft@gmail.com>
2022-11-03 20:53:38 +1100

    Update index.js

--- tablet-client/src/index.js
+++ tablet-client/src/index.js
@@ -19,13 +19,7 @@ root.render(
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDhKzDHg5o2_891FTSbGwr2RsCwM-cehGM",
-  authDomain: "familyconnect-a56a0.firebaseapp.com",
-  projectId: "familyconnect-a56a0",
-  storageBucket: "familyconnect-a56a0.appspot.com",
-  messagingSenderId: "352525739703",
-  appId: "1:352525739703:web:50f3611584192ea68a0f32",
-  measurementId: "G-6Q1VR8KNBG"
+  //removed to allow this repo to be set to public
 };
 
 // Initialize Firebase
